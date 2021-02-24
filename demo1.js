5d00fcd50b79f1a644ce21e039a0a0c331633fd6
karthik-siru <71187119+karthik-siru@users.noreply.github.com>
2021-02-24 20:04:15 +0530

    Delete demo1.js

--- demo1.js
+++
@@ -1,9 +0,0 @@
-function changestyling()
-{
-  var tag =  document.getElementsByClassName('mypara');// now tag will be an array of elements with class attribute mypara
-
-  for(var i = 0 ;i < tag.length ; i ++)
-  {
-    tag[i].style.color = "red";
-  }
-}
