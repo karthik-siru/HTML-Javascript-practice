e9111bc90203df8afa0261d1b8942fdab1a36fbc
karthik-siru <71187119+karthik-siru@users.noreply.github.com>
2021-02-26 19:56:41 +0530

    Update formvalidations.js

--- form-validations-2/formvalidations.js
+++ form-validations-2/formvalidations.js
@@ -1,13 +1,13 @@
 function validate ()
 {
-  var username  =  document.getElementById("user") ;
-  var pswd     =  document.getElementById("pass") ;
+  var username  =  document.getElementById("user") ; // takes the username from the input text box and stores it in username 
+  var pswd     =  document.getElementById("pass") ;  // similarly the password
 
    if (username.value.trim() == "")
    {
-     username.style.border = "solid 3px red";
-     document.getElementById("labeluser").style.visibility = "visible" ;
-     return false ;
+     username.style.border = "solid 3px red";  // makes the boder of the user box red .
+     document.getElementById("labeluser").style.visibility = "visible" ;  // pops the message which is already loaded in the label tag . 
+     return false ; // says the form validater that , they have entered wrong value . 
    }
    else if (pswd.value.trim() == "" )
   {
