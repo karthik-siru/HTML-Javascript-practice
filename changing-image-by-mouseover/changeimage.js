e2c3bc3c4ee356c2ec59f991c03dc0dd569b2327
karthik-siru <71187119+karthik-siru@users.noreply.github.com>
2021-02-27 17:51:35 +0530

    Add files via upload

--- changing-image-by-mouseover/changeimage.js
+++ changing-image-by-mouseover/changeimage.js
@@ -1,1 +1,19 @@
+function set_new_image()
+{
+   document.getElementById('img').src = "disthi.jpg" ;
+}
 
+function set_old_image()
+{
+   document.getElementById("img").src = "srihith.jpg";
+}
+
+function change_image ()
+{
+   document.getElementById("img").src = "disthi.jpg";
+}
+
+function change_to_normal()
+{
+  document.getElementById('img').src =  "srihith.jpg";
+}
